b6e6c98b7361899520cdf3d11c8e53462d6ca842
Datta Ghadage <dattaghadage9696.com>
2023-04-21 17:41:27 +0530

    counter1

---
+++ Counterapp/src/components/Counter.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1  class="count">Couter: {{ counter }}</h1>
+        <button @click="counter = counter + 1">INC</button>
+        <button @click="counter = counter - 1">DEC</button>
+    </div>
+</template>
+<script>
+export default {
+    name: "Counter",
+    data() {
+        return {
+            counter: 0
+        }
+    }
+}
+</script>
+<style>
+.count{
+   color: orange;
+}
+</style>
